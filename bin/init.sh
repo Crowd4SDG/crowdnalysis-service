32ef3c8fcb60135c799b49868be279057d7407e3
Oguz Mulayim <oguz@iiia.csic.es>
2021-12-25 18:21:31 +0100

    Add Stan models support (#1)

--- bin/init.sh
+++ bin/init.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+echo "Initializing requirements for crowdnalysis-service..."
+
+# Create a virtual env
 if [[ ! -d .venv ]]; then
     python3 -m venv .venv
     echo "Created a virtual environment at ${PWD}/.venv"
@@ -8,3 +11,18 @@ source .venv/bin/activate
 pip install -U pip
 pip install -r requirements.txt
 deactivate
+
+# Install CmdStan CLI
+CMDSTAN_DIR="$HOME/.cmdstan"
+CMDSTAN_VER="2.26.1"
+if [[ "$OSTYPE" == "linux-gnu"*  ||  "$OSTYPE" == "darwin"* ]]; then
+  if [[ ! -d "$CMDSTAN_DIR" ]]; then
+    echo "Installing CmdStan library into '${CMDSTAN_DIR}', please wait..."
+    install_cmdstan -d $CMDSTAN_DIR  -v $CMDSTAN_VER
+    echo "CmdStan library installed (v${CMDSTAN_VER})."
+  else
+    echo "CmdStan library already installed in '${CMDSTAN_DIR}'."
+  fi
+fi
+
+echo "Initialization completed."
